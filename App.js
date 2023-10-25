0e76d79bf492827e55cdbcb00cd9b1f4c3d5b15f
JongWon <danchew90@gmail.com>
2023-10-26 00:03:23 +0900

    icons

--- App.js
+++ App.js
@@ -3,10 +3,21 @@ import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const API_KEY = "36b289e5b9496ef9299f403a3e0a1c44";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Rain: "rains",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snowflake",
+  Drizzle: "rain",
+  Thunderstorm: "lightnings",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
@@ -51,7 +62,12 @@ export default function App() {
         ) : (
           days.map((day, index) => (
             <View style={styles.day} key={index}>
-              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+              <View
+                style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%" }}
+              >
+                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                <Fontisto name={icons[day.weather[0].main]} size={68} color={"white"} style={{ marginTop: 60 }} />
+              </View>
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyDesc}>{day.weather[0].description}</Text>
             </View>
@@ -77,16 +93,17 @@ const styles = StyleSheet.create({
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    padding: 20,
   },
   temp: {
-    fontSize: 168,
+    fontSize: 80,
     marginTop: 50,
     color: "#fff",
   },
   description: {
-    marginTop: -30,
-    fontSize: 60,
+    // marginTop: -30,
+    fontSize: 30,
     color: "#fff",
   },
   tinyDesc: {
